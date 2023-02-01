2b3f6df8c621c3c91ca6c53cd2e4aa738aa509d1
bahasaidi <baha140700m@gmail.com>
2023-02-01 11:09:04 +0100

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent] //point de demarage module eli bech ytlansa loul
+})
+export class AppModule { }
---
+++ src/app/core/modele/product.ts
@@ -0,0 +1,8 @@
+export  class  Product {
+  id!:number; //bech yntaha erreur na3ml ! =>tnajem na3mlo config fard mara f tn config.jeson
+  title!:string;
+  price!:number;
+  quantity!:number;
+  like!:number;
+
+}
